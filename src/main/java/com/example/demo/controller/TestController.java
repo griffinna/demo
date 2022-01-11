a1bad2304df662427235ee51bd0f771e92b9784a
Griffin <garam.dev9@gmail.com>
2022-01-11 22:36:05 +0900

    Example @PathVariable

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,11 @@ public class TestController {
     @GetMapping("/testGetMapping")
     public String testControllerWithPath() {
         return "Hello World! testGetMapping";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+        return String.format("Hello World! ID : %d", id);
     }
 
 }
