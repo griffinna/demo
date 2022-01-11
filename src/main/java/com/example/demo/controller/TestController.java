a3e189a2c6a6174fa76223290ba850e80025c936
Griffin <garam.dev9@gmail.com>
2022-01-11 22:33:48 +0900

    Create GetMapping Controller

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // http 관련 코드 및 요청/응답 매핑
+@RequestMapping("test") // resource
+public class TestController {
+
+    @GetMapping
+    public String testController() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath() {
+        return "Hello World! testGetMapping";
+    }
+
+}
