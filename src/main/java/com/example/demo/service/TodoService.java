c5d352298a27223bcd46c988211b0ed6b450015a
Griffin <garam.dev9@gmail.com>
2022-01-12 21:03:36 +0900

    [2.3.1] 서비스 구현 및 리팩토링

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -6,10 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j      // 로그 라이브러리 사용
 @Service    // 비즈니스 로직을 수행하는 서비스 레이어
 public class TodoService {
 
+    public static final String ENTITY_CANNOT_BE_NULL = "Entity cannot be null";
+    public static final String UNKNOWN_USER = "Unknown User.";
     @Autowired
     private TodoRepository repository;
 
@@ -22,5 +26,28 @@ public class TodoService {
         // TodoEntity 검색
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        // Validations
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id : {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn(ENTITY_CANNOT_BE_NULL);
+            throw new RuntimeException(ENTITY_CANNOT_BE_NULL);
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn(UNKNOWN_USER);
+            throw new RuntimeException(UNKNOWN_USER);
+        }
     }
 }
