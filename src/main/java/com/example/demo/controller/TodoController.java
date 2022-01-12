c33b005c76f2b375dfd25dff5e0e4b8572b1c4fe
Griffin <garam.dev9@gmail.com>
2022-01-12 21:21:49 +0900

    [2.3.2] Retrieve Todo 구현

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -60,4 +60,22 @@ public class TodoController {
             return ResponseEntity.badRequest().body(response);
         }
     }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String temporaryUserId = "temporary-user";  //temporary user id.
+
+        // 1. Todo 목록 조회
+        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+        // 2. 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        // 3. 변환된 TodoDTO 리스트를 이용해 ResponseDTO 초기화
+        ResponseDTO<TodoDTO> resopnse = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        // 4. ResponseDTO 리턴
+        return ResponseEntity.ok().body(resopnse);
+    }
+
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -50,4 +50,8 @@ public class TodoService {
             throw new RuntimeException(UNKNOWN_USER);
         }
     }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
 }
